bf60c55141f3fe8446c7d4f21c05900a2a2cd8c6
OscarInfanzon <111141384+OscarInfanzon00@users.noreply.github.com>
2025-02-12 12:19:34 -0500

    User List Finished

--- Collaborative Story Builder/Collaborative Story Builder/Assets/Scripts/Solo_UploadStory.cs
+++ Collaborative Story Builder/Collaborative Story Builder/Assets/Scripts/Solo_UploadStory.cs
@@ -9,6 +9,7 @@ using TMPro;
 
 public class UploadStory : MonoBehaviour
 {
+    private UserData userData;
     public TMP_InputField storyInputField;
     private FirebaseFirestore db;
 
@@ -17,6 +18,7 @@ public class UploadStory : MonoBehaviour
     void Start()
     {
         db = FirebaseFirestore.DefaultInstance;
+        userData = User.GetUser();
     }
 
     public void SaveStory()
@@ -32,13 +34,23 @@ public class UploadStory : MonoBehaviour
 
         string userID = user.UserId;
         string storyID = Guid.NewGuid().ToString();
+        string timestamp = Timestamp.GetCurrentTimestamp().ToString();
+
+        List<string> storyTexts = new List<string>
+    {
+        storyInputField.text
+    };
+
+        List<string> usersID = new List<string> { userID };
+        List<string> usersUsernames = new List<string> { userData.Username };
 
         Dictionary<string, object> storyData = new Dictionary<string, object>
     {
         { "storyID", storyID },
-        { "userID", userID },
-        { "storyText", storyInputField.text},
-        { "timestamp", Timestamp.GetCurrentTimestamp() }
+        { "storyTexts", storyTexts },
+        { "timestamp", timestamp },
+        { "usernames", usersUsernames },
+        { "users", usersID }
     };
 
         DocumentReference storyRef = db.Collection("Stories").Document(storyID);
@@ -47,8 +59,8 @@ public class UploadStory : MonoBehaviour
         {
             if (task.IsCompletedSuccessfully)
             {
-                notificationManager.Notify("Your story was succesfully uploaded!", 3f);
-                Debug.Log("Story saved successfully by user: " + userID);
+                notificationManager.Notify("Your story was successfully uploaded!", 3f);
+                Debug.Log("Story saved successfully with ID: " + storyID);
             }
             else
             {
