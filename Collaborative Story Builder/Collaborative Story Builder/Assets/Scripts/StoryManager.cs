86f48ec5cd4fc1989a1fc9a9e99ef3a0aea40644
OscarInfanzon <111141384+OscarInfanzon00@users.noreply.github.com>
2025-03-05 15:34:55 -0500

    Friends stories come first already working

--- Collaborative Story Builder/Collaborative Story Builder/Assets/Scripts/StoryManager.cs
+++ Collaborative Story Builder/Collaborative Story Builder/Assets/Scripts/StoryManager.cs
@@ -8,16 +8,50 @@ using Firebase.Auth;
 public class StoryManager : MonoBehaviour
 {
     FirebaseFirestore db;
+    FirebaseUser currentUser;
     public Transform storyListContainer;
     public GameObject storyCardPrefab;
     public GameObject StoryViewerUI;
     private string storyID;
-    
+    private HashSet<string> friendsSet = new HashSet<string>();
 
     private void Start()
     {
         db = FirebaseFirestore.DefaultInstance;
-        LoadStories();
+        currentUser = FirebaseAuth.DefaultInstance.CurrentUser;
+        LoadFriends();
+    }
+
+    void LoadFriends()
+    {
+        db.Collection("Friends").Document(User.GetUser().UserID).GetSnapshotAsync().ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCompletedSuccessfully)
+            {
+                DocumentSnapshot doc = task.Result;
+                if (doc.Exists)
+                {
+                    Dictionary<string, object> userData = doc.ToDictionary();
+                    if (userData.ContainsKey("friends"))
+                    {
+                        List<object> friendsList = (List<object>)userData["friends"];
+                        foreach (var friend in friendsList)
+                        {
+                            friendsSet.Add(friend.ToString());
+                        }
+                    }
+                    LoadStories(); 
+                }
+                else
+                {
+                    Debug.LogError("User document does not exist");
+                }
+            }
+            else
+            {
+                Debug.LogError("Failed to load friends: " + task.Exception);
+            }
+        });
     }
 
     void LoadStories()
@@ -28,6 +62,9 @@ public class StoryManager : MonoBehaviour
             {
                 try
                 {
+                    List<Story> friendStories = new List<Story>(); 
+                    List<Story> otherStories = new List<Story>(); 
+
                     foreach (DocumentSnapshot doc in task.Result.Documents)
                     {
                         Dictionary<string, object> data = doc.ToDictionary();
@@ -41,21 +78,29 @@ public class StoryManager : MonoBehaviour
                             }
                         }
 
-                        List<string> users = new List<string>();
+                        List<string> usersnames = new List<string>();
                         if (data.ContainsKey("usernames"))
                         {
                             foreach (var item in (List<object>)data["usernames"])
+                            {
+                                usersnames.Add(item.ToString());
+                            }
+                        }
+
+                        List<string> users = new List<string>();
+                        if (data.ContainsKey("users"))
+                        {
+                            foreach (var item in (List<object>)data["users"])
                             {
                                 users.Add(item.ToString());
                             }
                         }
 
                         storyID = (string)data["storyID"];
-                        DateTime timestamp = DateTime.MinValue; // Default value if timestamp is missing
+                        DateTime timestamp = DateTime.MinValue;
                         if (data.ContainsKey("timestamp"))
                         {
                             object timestampObj = data["timestamp"];
-
                             if (timestampObj is Firebase.Firestore.Timestamp firestoreTimestamp)
                             {
                                 timestamp = firestoreTimestamp.ToDateTime();
@@ -65,11 +110,47 @@ public class StoryManager : MonoBehaviour
                                 Debug.LogError($"Invalid timestamp format: {timestampObj.GetType()}");
                             }
                         }
-                        string previewText = storyTexts.Count > 0 ? storyTexts[0] : "No preview available";
-                        Story newStory = new Story(previewText, storyTexts, users, timestamp);
 
-                        CreateStoryCard(newStory);
+                        // Check if any user in the story is a friend
+                        bool isFriend = false;
+                        foreach (var user in users)
+                        {
+                            if (friendsSet.Contains(user)) // If user is a friend
+                            {
+                                isFriend = true;
+                                break; // Stop checking once we find a match
+                            }
+                        }
+
+                        string previewText = storyTexts.Count > 0 ? storyTexts[0] : "No preview available";
+                        Story newStory = new Story(previewText, storyTexts, usersnames, timestamp);
+
+                        if (isFriend)
+                        {
+                            friendStories.Add(newStory); // Add to friend stories
+                        }
+                        else
+                        {
+                            otherStories.Add(newStory); // Add to other stories
+                        }
                     }
+
+                    // Sort stories by timestamp (latest first)
+                    friendStories.Sort((s1, s2) => s2.timestamp.CompareTo(s1.timestamp));
+                    otherStories.Sort((s1, s2) => s2.timestamp.CompareTo(s1.timestamp));
+
+                    // Display friend stories first, followed by other stories
+                    foreach (Story story in friendStories)
+                    {
+                        CreateStoryCard(story, true);
+
+                    }
+
+                    foreach (Story story in otherStories)
+                    {
+                        CreateStoryCard(story, false);
+                    }
+
                 }
                 catch (Exception e)
                 {
@@ -83,13 +164,15 @@ public class StoryManager : MonoBehaviour
         });
     }
 
-
-    void CreateStoryCard(Story story)
+    // Create and display story cards
+    void CreateStoryCard(Story story, bool areFriends)
     {
         GameObject newCard = Instantiate(storyCardPrefab, storyListContainer);
         newCard.GetComponent<StoryCardUI>().StoryViewerUI = StoryViewerUI;
         StoryCardUI cardUI = newCard.GetComponent<StoryCardUI>();
         cardUI.SetStoryInfo(story);
+        if(areFriends)
+            cardUI.activateFriends();
         cardUI.storyID = storyID;
     }
 }
