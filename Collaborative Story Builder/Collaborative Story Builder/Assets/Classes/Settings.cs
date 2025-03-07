0dc6a027a59b69511b0af589e2c259f7b1df5ad3
shiitake2200 <141689097+shiitake2200@users.noreply.github.com>
2025-03-07 12:46:57 -0500

    MainMenu - All managers are under Manager parent object in Hierarchy

---
+++ Collaborative Story Builder/Collaborative Story Builder/Assets/Classes/Settings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public static class Settings
+{
+    public static float musicVolume;
+    public static string inputLangTranslation;
+    public static string outputLangTranslation;
+
+}
